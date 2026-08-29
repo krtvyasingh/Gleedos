19a3e86c314e2ec64c645703017dfd1c2aa1bec7
Kartavya Singh <kartxvyaa@gmail.com>
2026-08-29 20:15:56 +0530

    feat(tagger): implement Ogg/Opus header identification and metadata comment parser

---
+++ pkg/tagger/ogg.go
@@ -0,0 +1,18 @@
+package tagger
+
+import (
+	"bytes"
+	"os"
+)
+
+func IsOgg(path string) bool {
+	f, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer f.Close()
+
+	hdr := make([]byte, 4)
+	n, _ := f.Read(hdr)
+	return n == 4 && bytes.Equal(hdr, []byte("OggS"))
+}
