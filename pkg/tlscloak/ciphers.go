9e755a1be154449855764500263e6bffeec495bd
Kartavya Singh <kartxvyaa@gmail.com>
2026-09-07 19:40:13 +0530

    security(tlscloak): add custom cipher suite ordering configuration

---
+++ pkg/tlscloak/ciphers.go
@@ -0,0 +1,11 @@
+package tlscloak
+
+import "crypto/tls"
+
+func GetModernCipherSuites() []uint16 {
+	return []uint16{
+		tls.TLS_AES_128_GCM_SHA256,
+		tls.TLS_AES_256_GCM_SHA384,
+		tls.TLS_CHACHA20_POLY1305_SHA256,
+	}
+}
