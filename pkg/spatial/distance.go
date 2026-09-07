3e4d929a64d86be60719b9b71951f67d92a0d44f
Kartavya Singh <kartxvyaa@gmail.com>
2026-09-07 19:09:22 +0530

    feat(spatial): implement distance attenuation model for 3D audio rendering

---
+++ pkg/spatial/distance.go
@@ -0,0 +1,13 @@
+package spatial
+
+import "math"
+
+func CalculateDistanceGain(distanceMeters, referenceDistance, maxDistance float64) float64 {
+	if distanceMeters <= referenceDistance {
+		return 1.0
+	}
+	if distanceMeters >= maxDistance {
+		return 0.0
+	}
+	return referenceDistance / (referenceDistance + (distanceMeters - referenceDistance))
+}
